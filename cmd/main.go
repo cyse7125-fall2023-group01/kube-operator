51e8f519ac407926188cd2ce0267f1bcb0943f4a
Ab <gade.ab@northeastern.edu>
2023-12-08 15:19:34 -0500

    fix(rbac): Changed Rbacs

--- cmd/main.go
+++ cmd/main.go
@@ -32,8 +32,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
-	webappv1 "my.domain/guestbook/api/v1"
-	"my.domain/guestbook/internal/controller"
+	monitoringv1alpha1 "httpcheck.io/api/v1alpha1"
+	"httpcheck.io/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -45,7 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(webappv1.AddToScheme(scheme))
+	utilruntime.Must(monitoringv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -71,7 +71,8 @@ func main() {
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "ecaf1259.my.domain",
+		LeaderElectionID:       "0538282b.httpcheck.io",
+
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
@@ -89,11 +90,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.GuestbookReconciler{
+	if err = (&controller.CronJobReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Guestbook")
+		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
